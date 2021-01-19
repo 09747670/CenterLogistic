358aeb9cfafae6a9680aea50faf27fa530fa349c
Vitaliy <vitaliy.sh.82@gmail.com>
2021-01-19 19:22:24 +0200

    Function ScrollSpy

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(function() {
     let header = $("#header");
     let introH = intro.innerHeight();
     let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
 
 
 /*_______________Header scroll____________________ */
@@ -42,8 +43,41 @@ $(function() {
            scrollTop:  scrollElPos - headerH
         }, 500)
 
-    })
+    });
 
+/*_______________ScrolSpy____________________ */
+    let windowH = $(window).height();
 
-})
+    scrollSpy(scrollTop);
+
+    $(window).on("scroll", function(){
+
+            scrollTop = $(this).scrollTop();
+
+            scrollSpy(scrollTop);
+
+        });
+
+        function scrollSpy(scrollTop){
+
+            $("[data-scrollspy]").each(function(){
+            let $this = $(this);
+            let sectionId = $this.data("scrollspy");
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.3);
+
+            if(scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+
+            if(scrollTop == 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+
+        })
+
+    }
+
+});
 
